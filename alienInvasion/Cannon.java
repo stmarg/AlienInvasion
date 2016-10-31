8f7dc1ab72609902863532e29ba83e474b72e615
buklaodee <jasonkim0708@gmail.com>
2016-10-31 08:15:30 -0700

    cannon.java not updated, no compilier

---
+++ alienInvasion/Cannon.java
@@ -0,0 +1,45 @@
+
+public class Cannon
+{
+	private int aradius;
+	private double asize;
+	private int x;
+	private int y;
+	private int bwidth;
+	private int blength;
+	
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public int getWidth()
+	{
+		return bwidth;
+	}
+	public int getLength()
+	{
+		return blength;
+	}
+	public int setAradius;
+	{
+		this.aradius = 1;
+	}
+	
+	public double setArea;
+	{
+		this.asize = (3.14159*aradius*aradius)/2;
+	}
+	
+	public int setAlocation;
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	
+}
