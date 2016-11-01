9376ad2030c513a00c638a1b65f6426de06614d2
charliesmith381 <charles.smith@smes.org>
2016-11-01 09:19:44 -0700

    Simpler Missile class

--- alienInvasion/src/Missile.java
+++ alienInvasion/src/Missile.java
@@ -1,88 +1,33 @@
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.GraphicsContext; 
 
 public class Missile
 {
-	private double xPos, yPos, speed;
-	private double diameter = 50;
+	private double xPos, yPos;
 	private boolean DidHit;
 	
-	double angle = Math.atan(yPos / xPos);
 	
-	public void exist(GraphicsContext gc, SpaceShip s)
+	public void Missile (GraphicsContext gc, double startingxPos, double startingyPos, double speed, double angle, double diameter)
 	{
-		if (DidHit == false)
+		this.xPos = startingxPos;
+		this.yPos = startingyPos;
+		
+		while (DidHit == false)
 		{
 			gc.fillOval(xPos, yPos, diameter, diameter);
+			this.xPos = this.xPos + speed * Math.cos(angle);
+			this.yPos = this.yPos + speed * Math.sin(angle);
 		}
-			
-	}
-	
-	public void setSpeed(double speed)
-	{
-		this.speed = speed;
-	}
-	
-	public void setPos(double angle, double cannon_length)
-	{
-		xPos = 700 + cannon_length * Math.cos(angle);
-		yPos = 800 + cannon_length * Math.sin(angle); 
 		
 	}
 	
-	public void shootMissile(double angle, Ship s)
+	public boolean getDidHit()
 	{
-		boolean test = false;
-		double xV = 0;
-		double yV = 0;
-		boolean midstep = false;
-		
-			xV = this.speed * Math.cos(angle);
-			yV = this.speed * Math.sin(angle);
-			
-			
-		if (test == true)
-		{
-			midstep = true;
-		}
-			
-		if (midstep == true)
-		{
-			xPos = xPos + xV;
-			yPos = yPos + yV;
-		}
-		
-		if (this.xPos > s.getxPos() && this.xPos < (s.getxPos() + s.getLength()) && this.yPos > s.getyPos() && this.yPos < (s.getyPos() + s.getHeight()))
-		{
-			//this.explode();
-		}
-			
-	}
-	
-	public void explode(GraphicsContext gc)
-	{
-			
-		gc.fillOval(xPos, yPos, diameter /4, diameter /4); //WIP
-			
-			
-	}
-	
-	public boolean didHit(Ship s)
-	{
-		boolean mid = false;
-		
-		if (this.xPos > s.getxPos() && this.xPos < (s.getxPos() + s.getLength()) && this.yPos > s.getyPos() && this.yPos < (s.getyPos() + s.getHeight()))
-		{
-			mid = true;
-		}	
-		
-		if (mid == true)
-		{
-			DidHit = true;
-		}
-			
 		return DidHit;
-		
 	}
 	
+	public void setDidHit(boolean dh)
+	{
+		this.DidHit = dh;
+	}
 	
-}
+}	
