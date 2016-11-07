e2b74376977d90fb20318221093e3ba343d858bd
charliesmith381 <charles.smith@smes.org>
2016-11-07 08:16:13 -0800

    changed missile class and charlie's game

--- alienInvasion/src/CharlieGame.java
+++ alienInvasion/src/CharlieGame.java
@@ -1,9 +1,58 @@
-public class CharlieGame
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import tsinn.ui.SimpleApp;
+
+public class CharlieGame extends SimpleApp
 {
-	public static void main (String[] args)
-	{
-		System.out.println("Charlie's game");
+	boolean test = true;
+	
+	Missile[] bullets = new Missile[5];
+	
+	Building B = new Building(400, 400, 50, Color.BLUE, Color.BEIGE);
+	
+	Missile M = new Missile(400, 700, false, 50);
+	
+	public void onMousePressed(MouseEvent me)
+	{	
+		
+		if (me.getX() > 0 && me.getY() > 0)
+		{
+			test = false;
+		}
+	}
+
+	@Override
+	public void updateAnimation(long arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+		
+		B.draw(gc);
+		M.draw(gc);
+		if (test = true)
+		{
+			M.shoot(3, 90);
+		}
+		
 		
 		
 	}
+
+	@Override
+	public void setupApp(GraphicsContext arg0) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	public static void main (String [] args)
+	{
+		launch();
+	}
+	
 }
