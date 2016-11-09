4506d35f6d4efa2a1633123daba5a2bead749305
Linna-cubbage <Linna.cubbage@smes.org>
2016-11-09 10:56:51 -0800

    updated some code

---
+++ alienInvasion/src/LinnaGame.java
@@ -0,0 +1,17 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+
+//I'm so pleased that my commits actually seem to be working now!!!
+
+public class LinnaGame extends SimpleApp
+{
+	//Building[] buildings = new Building [a number]
+	//Ship[] ships = new Ship [a number]
+	//private Cannon c
+	
+	
+	public static void main (String[] args)
+	{
+		System.out.println("Just to update????");
+	}
+}
