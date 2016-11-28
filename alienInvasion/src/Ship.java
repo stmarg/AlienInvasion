54129ff79bf6af60429479102cf410954f64d1f2
Kevinland123 <Kevin691827@gmail.com>
2016-11-28 10:22:58 -0800

    something

--- alienInvasion/src/Ship.java
+++ alienInvasion/src/Ship.java
@@ -96,17 +96,12 @@ public class Ship {
 		double bRight = b.getX() + b.getS();
 		double bTop = b.getY();
 		double bBottom = b.getY() + b.getS();
-		
+
 		return sRight > bLeft && sLeft < bRight && sBottom > bTop && sTop < bBottom;
 
-		/*if (sRight > bLeft && sLeft < bRight && sBottom > bTop && sTop < bBottom) {
-			return true;
-		} else {
-			return false;
-		}
-		*/
-		
-		//return (abs(s.x + s.w/2) - b.x + (b.w/2)) < (s.w + b.w)/2 && abs(s.y + (s.h/2) - b.y + (b.h/2) < (s.b + b.h)/2)
+		// return (Math.abs(this.x + this.width/2) - b.getX() + (b.getS()/2)) <
+		// (s.w + b.getS())/2 && abs(s.y + (s.h/2) - b.y + (b.getS()/2) < (s.b +
+		// b.getS())/2)
 	}
 
 	public void setLocation(int x, int y) {
