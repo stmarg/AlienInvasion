ede168657f90580d196ade1ea5ead3ede58c261a
charliesmith381 <charles.smith@smes.org>
2016-12-07 17:53:47 -0800

    made a pause function and made ship spawning better

--- alienInvasion/src/CharlieGame.java
+++ alienInvasion/src/CharlieGame.java
@@ -8,22 +8,15 @@ import java.util.ArrayList;
 public class CharlieGame extends SimpleApp
 {
 	
-	//boolean test = false;
 	double count = 0;
-	
-	//double angle = 90;
-	
-	//Missile[] bullets = new Missile[ammo];
-	
-	//Missile[] bullets = new Missile[10];
+	double limit = 0;
+	boolean paused = false;
 	
 	Building[] buildings = new Building[16];
 	
 	ArrayList <Missile> missiles = new ArrayList<> ();
 	
 	ArrayList <Ship> ships = new ArrayList<> ();
-	
-	//Missile M = null;
 	
 	Cannon C = new Cannon(700, 800, 100, 100, 90);
 	
@@ -51,36 +44,28 @@ public class CharlieGame extends SimpleApp
 			//this.angle = this.angle - 22.5;
 		}*/
 		
-		if (ke.isShiftDown() == true)
+		if (ke.isShiftDown() == true && paused == false)
 		{
-			
-			//M = new Missile(C.getX(), C.getY(), 50, angle, 4);
-			
-			//bullets = new Missile[ammo];
-			
-			/*for (int i = 0; i < bullets.length; i++)
-			{
-				bullets[i] = new Missile(22.5 * i);
-			}*/
-			
-			
-			missiles.add(new Missile(C.getX(), C.getY(), 50, C.getAngle(), 20, Color.BLACK));
+				
+			missiles.add(new Missile(C.getX(), C.getY(), 50, C.getAngle(), 2, Color.BLACK));
 			//missiles.add(C.shoot());
-			
-			//ships.add(new Ship(600, 100, 100, 100));
-			
-			
-			
-			//test = true;
-			//count += 1;
+
+		}
+		
+		if (ke.isControlDown() == true)
+		{
+			if (paused == false)
+			{
+				paused = true;
+			} else
+			{
+				paused = false;
+			}
 			
 		}
 		
-		
 	}
 	
-	
-
 	@Override
 	public void updateAnimation(long arg0) {
 		// TODO Auto-generated method stub
@@ -91,88 +76,62 @@ public class CharlieGame extends SimpleApp
 	public void draw(GraphicsContext gc) {
 		// TODO Auto-generated method stub
 		
-		count = count + (int)(10 * Math.random());
-		
-		
-		/*if (M != null)
-		
-		//Why are you using M and missiles?
-		if (M != null)
+		if (!paused)
 		{
-			M.draw(gc);
-			
-			if (M.getyPos() + M.getDiameter() < 0)
-			{
-				M = null;
-			}
-		}*/
 		
-		/*for (int i = 0; i < ammo; i++)
-		{
-			bullets[i].draw(gc);
-		}*/
+		count++;
 		
-		
-		if (count % 150 == 0)
+		if (count >= limit)
 		{
 			ships.add(new Ship((int)(this.getWidth() * Math.random()), 0, 100, 100));
+			limit = limit + (int)((100 - count/30) * Math.random());
 		}
-		
-		
-		
 		
 		gc.fillText("Lives: " + lives, 50, 50);
 		
-		
 		C.draw(gc);
-		
-		
-		
-		for (int i = 0; i < missiles.size(); i++)
-		{
-			Missile m = missiles.get(i);
-			
-			m.fall(.5);
-			//m.wallbounce(0, getWidth());
-			
-			if (m.getyPos() < -m.getDiameter())
-			{
-				missiles.remove(m);
-			}
-		
-			if (m.isActive())
-			{
-			m.draw(gc);
 			
 			for (int k = 0; k < ships.size(); k++)
 			{
 				Ship s = ships.get(k);
-			
-				if (m.didHit(s))
+				
+				for (int i = 0; i < missiles.size(); i++)
 				{
-					m.setInactive(true);
+					Missile m = missiles.get(i);
 					
-					ships.remove(k);
+					//m.fall(.05);
+					//m.wallbounce(0, getWidth());
 					
-					break;
+					if (m.getyPos() < -m.getDiameter())
+					{
+						missiles.remove(m);
+						i--;
+					}
+				
+					if (m.isActive())
+					{
+						m.draw(gc);
+			
+						if (m.didHit(s))
+						{
+						m.setInactive(true);
+						
+							ships.remove(k);
+					
+							break;
+						}
+					}
 				}
 				
 				for (int j = 0; j < buildings.length; j++)
 				{
-					
 					if (s.didHit(buildings[j]))
 					{
 						ships.remove(k);
 						buildings[j].setAlive(false);
 						lives--;
 					}
-				}
-				
-			
-			}
-			
-			}
-			
+				}	
 		}
 		
 		if (lives <= 0)
@@ -180,82 +139,34 @@ public class CharlieGame extends SimpleApp
 			gc.fillText("GAME OVER", this.getWidth()/2, this.getHeight()/2);
 		}
 		
-		
-		
-		
 		for (Ship s : ships)
 		{
 			s.draw(gc);
 			s.move();
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*if (test == true)
-		{
-			if (count >= 0)
-			{
-				bullets[0].draw(gc);
-			}
-			if (count >= 1)
-			{
-				bullets[1].draw(gc);
-			}
-			if (count >= 2)
-			{
-				bullets[2].draw(gc);
-			}
-			if (count >= 3)
-			{
-				bullets[3].draw(gc);
-			}
-			if (count >= 4)
-			{
-				bullets[4].draw(gc);
-			}
-			
-		}*/
-		
-		
-		
 		for (int i = 1; i < buildings.length; i++)
 		{
 			buildings[i].draw(gc);
 		}
 		
-		
-	}
+		}
+		else
+		{
+			gc.fillText("PAUSED", getWidth()/2, getHeight()/2);
+		}
+	}	
 
 	@Override
 	public void setupApp(GraphicsContext arg0) {
 		// TODO Auto-generated method stub
-		
-		
 		
 		for (int i = 0; i < buildings.length; i++)
 		{
 			buildings[i] = new Building(getWidth() * i / buildings.length, getHeight() - 50, 50, Color.BLACK, Color.BURLYWOOD);
 		}
 		
-		
-		
-		
-		
 	}
-	
-	
 	
 	public static void main (String [] args)
 	{
